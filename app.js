f33dd4e427f74dbe0a6ef0b681ef211984e40be0
Zunaith Shariff <sheriffgaming926@gmail.com>
2024-12-02 17:47:48 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
   response.sendFile("./index.html",{root:__dirname});
 });
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app
